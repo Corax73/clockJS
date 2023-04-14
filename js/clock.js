26230538c7d104b9cf718f2ec8c3bd9080d9e57f
Corax73 <ramaz999@inbox.ru>
2023-04-14 22:56:30 +0300

    refact format time

--- js/clock.js
+++ js/clock.js
@@ -7,12 +7,15 @@ function getTime() {
     let minutes = date.getUTCMinutes();
     let hours = date.getHours();
     let seconds = date.getSeconds();
-    clockTitle.innerHTML = `${hours}:${minutes}:${seconds}`;
+    clockTitle.innerHTML = `${hours < 10 ? `0${hours}` : hours}:${minutes < 10 ? `0${minutes}` : minutes}:${seconds < 10 ? `0${seconds}` : seconds}`;
+
 }
 
 function init() {
 
     getTime();
+    setInterval(getTime, 1000)
+
 }
 
 init();
